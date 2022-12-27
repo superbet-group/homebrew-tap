df14b68aa8535600ca5cf1a2383fc4ace947b94b
Futa <99179596+ifutivic@users.noreply.github.com>
2022-12-27 21:06:20 +0100

    Update and rename private_strategy.rb to custom_download_strategy.rb

--- custom_download_strategy.rb
+++ custom_download_strategy.rb
@@ -1,32 +1,24 @@
 class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
   require "utils/formatter"
   require "utils/github"
-
-  def initialize(name, resource)
+def initialize(url, name, version, **meta)
     super
     parse_url_pattern
     set_github_token
   end
-
   def parse_url_pattern
-    url_pattern = %r{https://github.com/([^/]+)/([^/]+)/(\S+)}
-    unless @url =~ url_pattern
+    unless match = url.match(%r{https://github.com/([^/]+)/([^/]+)/(\S+)})
       raise CurlDownloadStrategyError, "Invalid url pattern for GitHub Repository."
     end
-
-    _, @owner, @repo, @filepath = *@url.match(url_pattern)
+    _, @owner, @repo, @filepath = *match
   end
-
   def download_url
     "https://#{@github_token}@github.com/#{@owner}/#{@repo}/#{@filepath}"
   end
-
-  def _fetch
-    curl download_url, "-C", downloaded_size, "-o", temporary_path
-  end
-
   private
-
+  def _fetch(url:, resolved_url:, timeout:)
+    curl_download download_url, to: temporary_path
+  end
   def set_github_token
     @github_token = ENV["HOMEBREW_GITHUB_API_TOKEN"]
     unless @github_token
@@ -34,57 +26,52 @@ class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
     end
     validate_github_repository_access!
   end
-
   def validate_github_repository_access!
     # Test access to the repository
     GitHub.repository(@owner, @repo)
-  rescue GitHub::HTTPNotFoundError
-    # We only handle HTTPNotFoundError here,
-    # becase AuthenticationFailedError is handled within util/github.
-    message = <<-EOS.undent
-        HOMEBREW_GITHUB_API_TOKEN can not access the repository: #{@owner}/#{@repo}
-        This token may not have permission to access the repository or the url of formula may be incorrect.
+  rescue GitHub::API::HTTPNotFoundError
+    # We switched to GitHub::API::HTTPNotFoundError,
+    # because we can now handle bad credentials messages
+    message = <<~EOS
+      HOMEBREW_GITHUB_API_TOKEN can not access the repository: #{@owner}/#{@repo}
+      This token may not have permission to access the repository or the url of formula may be incorrect.
     EOS
     raise CurlDownloadStrategyError, message
   end
 end
 
 class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDownloadStrategy
+  def initialize(url, name, version, **meta)
+    super
+  end
   def parse_url_pattern
     url_pattern = %r{https://github.com/([^/]+)/([^/]+)/releases/download/([^/]+)/(\S+)}
     unless @url =~ url_pattern
       raise CurlDownloadStrategyError, "Invalid url pattern for GitHub Release."
     end
-
     _, @owner, @repo, @tag, @filename = *@url.match(url_pattern)
   end
-
   def download_url
     "https://#{@github_token}@api.github.com/repos/#{@owner}/#{@repo}/releases/assets/#{asset_id}"
   end
-
-  def _fetch
+  private
+  def _fetch(url:, resolved_url:, timeout:)
     # HTTP request header `Accept: application/octet-stream` is required.
     # Without this, the GitHub API will respond with metadata, not binary.
-    curl download_url, "-C", downloaded_size, "-o", temporary_path, "-H", "Accept: application/octet-stream"
+    curl_download download_url, "--header", "Accept: application/octet-stream", to: temporary_path
   end
-
-  private
-
   def asset_id
     @asset_id ||= resolve_asset_id
   end
-
   def resolve_asset_id
     release_metadata = fetch_release_metadata
     assets = release_metadata["assets"].select { |a| a["name"] == @filename }
     raise CurlDownloadStrategyError, "Asset file not found." if assets.empty?
-
-    assets.first["id"]
+   assets.first["id"]
   end
-
   def fetch_release_metadata
-    release_url = "https://api.github.com/repos/#{@owner}/#{@repo}/releases/tags/#{@tag}"
-    GitHub.open(release_url)
+    #release_url = "https://api.github.com/repos/#{@owner}/#{@repo}/releases/tags/#{@tag}"
+    #GitHub::API.open_rest(release_url)
+    GitHub.get_release(@owner, @repo, @tag)
   end
 end
