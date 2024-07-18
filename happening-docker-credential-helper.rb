bfff204cae9ede4f2089f72feff8761dd1524bb6
patresso <me@patresso.io>
2024-07-18 22:59:47 +0200

    set right url, sha and homepage params

--- happening-docker-credential-helper.rb
+++ happening-docker-credential-helper.rb
@@ -1,13 +1,13 @@
 require_relative "custom_download_strategy"
 class HappeningDockerCredHelper < Formula
   desc "Docker credential helper for accessing ECR repos via teleport.happening.dev"
-  homepage ""
+  homepage "https://github.com/superbet-group/sre.docker-credential-helper"
   version "0.0.1"
 
   depends_on "jq"
   
-  url "https://github.com/superbet-group/engprod.lola/releases/download/v0.0.29/lola-macos-amd64.bundle.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "d0c372dbd7d3ebee7a8ed3a358a4d93ea416ea226907fe07cc97fd64af1bf393"
+  url "https://github.com/superbet-group/sre.docker-credential-helper/archive/refs/tags/0.0.1.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "42715fa310854a7db574b11be133bab028bbbeee0c7edcd52cef2ed18304832c"
   
   def install
     bin.install "docker-credential-teleport-happening-ecr"
