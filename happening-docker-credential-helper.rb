6ce6a1512c27531beff017b7cec1ad785845dc73
patresso <me@patresso.io>
2024-07-30 17:39:32 +0200

    update docker creds helper to 0.0.3

--- happening-docker-credential-helper.rb
+++ happening-docker-credential-helper.rb
@@ -1,13 +1,13 @@
 require_relative "custom_download_strategy"
-class HappeningDockerCredHelper < Formula
+class HappeningDockerCredentialHelper < Formula
   desc "Docker credential helper for accessing ECR repos via teleport.happening.dev"
   homepage "https://github.com/superbet-group/sre.docker-credential-helper"
-  version "0.0.1"
+  version "0.0.3"
 
   depends_on "jq"
   
-  url "https://github.com/superbet-group/sre.docker-credential-helper/archive/refs/tags/0.0.1.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "42715fa310854a7db574b11be133bab028bbbeee0c7edcd52cef2ed18304832c"
+  url "https://github.com/superbet-group/sre.docker-credential-helper/releases/download/0.0.3/docker-credential-helper.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "6ee0a787bcd46e73c9eebabeee1b68b40440010297641ded6a644ffa7f2cfc48"
   
   def install
     bin.install "docker-credential-teleport-happening-ecr"
