80cf62f8b9c01ec9280a8449c3e2dbe523266a5f
patresso <me@patresso.io>
2024-09-24 15:13:47 +0200

    teleport-cli 15.4.19

--- Casks/teleport-cli.rb
+++ Casks/teleport-cli.rb
@@ -3,8 +3,8 @@ cask "teleport-cli" do
   desc "Access proxy providing access to k8s, aws, apps, dbs and servers through ssh and rdp"
   homepage "https://goteleport.com/"
 
-  version "14.3.26"
-  sha256 "268ff986583a4ac6a7ea8406ae87652f958b275b0e63926065ec79c0a0ff1671"
+  version "15.4.19"
+  sha256 "716f55e941daf58c452d89cc878d96bcf850413b5680862149c5f79bc23bd569"
 
   url "https://cdn.teleport.dev/teleport-ent-#{version}.pkg",
       verified: "cdn.teleport.dev/"
