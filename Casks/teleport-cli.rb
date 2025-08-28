701225e6668b2ff736f92ed8957db9d87a0e5beb
Peter Kubicsek <peter.kubicsek@happening.xyz>
2025-08-28 16:57:23 +0200

    fix(SPRI-1033): remove deprecated conflicts_with and fix formatting
    - Remove deprecated conflicts_with method that causes brew warnings
    - Fix formula formatting according to brew style guidelines
    - Shorten description to comply with Homebrew standards

--- Casks/teleport-cli.rb
+++ Casks/teleport-cli.rb
@@ -1,21 +1,17 @@
 cask "teleport-cli" do
-  name "Teleport enterprise utilities"
-  desc "Access proxy providing access to k8s, aws, apps, dbs and servers through ssh and rdp"
-  homepage "https://goteleport.com/"
-
   version "17.5.2"
   sha256 "5c89b293f0f914fe35ceb04995d99ef0e91a54182833be4acae23472ce041d59"
 
   url "https://cdn.teleport.dev/teleport-ent-#{version}.pkg",
       verified: "cdn.teleport.dev/"
+  name "Teleport enterprise utilities"
+  desc "Access proxy for k8s, aws, apps, dbs and servers via ssh/rdp"
+  homepage "https://goteleport.com/"
 
   livecheck do
     url "https://goteleport.com/download/"
     regex(/teleport-ent[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
   end
-
-  conflicts_with cask:    "tsh",
-                 formula: "teleport"
 
   pkg "teleport-ent-#{version}.pkg"
 
