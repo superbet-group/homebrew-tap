7c78546fd9b9658977c7fe48d9f80835aa3c6a56
Maxwell Leonardo <maxwell.leonardo@happening.xyz>
2025-09-30 15:37:22 -0300

    Applying KISS and no renaming

--- observability-cli.rb
+++ observability-cli.rb
@@ -4,13 +4,13 @@ class ObservabilityCli < Formula
   homepage "https://github.com/superbet-group/observability.application.observability-cli"
   version "1.4.4"
 
+  @archive_file = nil
+  @sha256_checksum = nil
   on_macos do
     if Hardware::CPU.arm?
-      @binary_name = "observability-cli"
       @archive_file = "observability-cli-macos-arm64.tar.gz"
       @sha256_checksum = "15febc77d2ce41b64fbd264373c1a9296fc2f07de14e232ba8d024783779ae92"
     elsif Hardware::CPU.intel?
-      @binary_name = "observability-cli"
       @archive_file = "observability-cli-macos-amd64.tar.gz"
       @sha256_checksum = "4b88a3b34222a0f143e9127218f25c37c43d62cecb90d9c504fe0335f1316b58"
     end
@@ -18,11 +18,9 @@ class ObservabilityCli < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      @binary_name = "observability-cli"
       @archive_file = "observability-cli-linux-amd64.tar.gz"
       @sha256_checksum = "4830acf9fa0bbb42a6a5492739dde334e42f2de90d423694a5513900c60facb6"
     elsif Hardware::CPU.arm?
-      @binary_name = "observability-cli"
       @archive_file = "observability-cli-linux-arm64.tar.gz"
       @sha256_checksum = "e72f43813d440ef9f4c2963fc7f6ca24a9ff6fd51a4ce244c5e981de77d316ed"
     end
@@ -34,11 +32,7 @@ class ObservabilityCli < Formula
   sha256 @sha256_checksum
 
   def install
-    bin.install @binary_name => "observability-cli"
+    bin.install "observability-cli"
     chmod "+x", bin/"observability-cli"
-  end
-
-  test do
-    system "#{bin}/observability-cli", "--help"
   end
 end
