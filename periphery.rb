58f4941b09442b782df18467710a26ceef5b4d4b
Igor Orecic <igor.orecic@super.xyz>
2026-09-04 14:19:25 +0200

    feat: point to new version of periphery lib

--- periphery.rb
+++ periphery.rb
@@ -4,13 +4,13 @@
 class Periphery < Formula
   desc "Tool to identify unused code in Swift projects"
   homepage "https://github.com/superology-ios/periphery"
-  version "3.7.7"
+  version "3.7.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/superology-ios/periphery/releases/download/3.7.7/periphery-3.7.7-arm64.tar.gz"
-      sha256 "5422a686fcbc7ca0e69add3e64960b333b9682823a0f4f49f6bab897345be9c1"
+      url "https://github.com/superology-ios/periphery/releases/download/3.7.8/periphery-3.7.8-arm64.tar.gz"
+      sha256 "802ad6932f9a72f55a93200a7edd8e63ab3a0a7632b23426f3e4bd16a53bc4ed"
 
       define_method(:install) do
         bin.install "periphery"
